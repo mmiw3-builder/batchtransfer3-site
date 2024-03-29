24a7fbe07842cf87d7b948710a8631e6757d121d
Azleal <liyao.mr@gmail.com>
2024-03-29 12:06:25 +0800

    transfer

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,6 @@
-'use client'
-import React from 'react'
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Tabs, Tab, Card, CardBody} from "@nextui-org/react";
-import TransferEther from './components/TransferEther';
-import TransferERC20 from './components/TransferERC20';
-import { useAccount, useBalance, useChains } from 'wagmi';
-import Image from "next/image";
-import Donate from './components/Donate';
-import Footer from './components/Footer';
+import { redirect } from 'next/navigation';
 
 
-
-export default function Page() {
-
-  return (
-    <div>
-      <div style={{height: '100vh'}}>
-          批量转账
-      </div>
-    </div>
-  )
+export default function Home() {
+  redirect('/transfer');
 }
